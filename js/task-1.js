d1883008da399cca2f67f117f70cd4a297d9c7cb
oxfox1986 <145785269+oxfox1986@users.noreply.github.com>
2023-12-23 14:31:33 +0100

    готово

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-   const totalPrice = quantity * pricePerDroid;
-   if (totalPrice>customerCredits){
-    return "Insufficient funds!";}
-   else {return `You ordered ${quantity} droids worth ${totalPrice} credits!`;}
+  const totalPrice = quantity * pricePerDroid;
+  return totalPrice > customerCredits
+    ? "Insufficient funds!"
+    : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
 }
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
